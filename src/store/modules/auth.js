530a68b63aa1d915b8da9455407ecd7b8de42d99
ministrov <61511030+ministrov@users.noreply.github.com>
2023-01-31 13:47:04 +0300

    feat: create Login page

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -8,35 +8,79 @@ const state = {
   isLoggedIn: null
 }
 
+export const mutationTypes = {
+  registerStart: '[auth] registerStart',
+  registerSuccess: '[auth] registerSucces',
+  registerFailure: '[auth] registerFailure',
+
+  loginStart: '[auth] loginStart',
+  loginSuccess: '[auth] loginSucces',
+  loginFailure: '[auth] loginFailure',
+}
+
+export const actionTypes = {
+  register: '[auth] register',
+  login: '[auth] login'
+}
+
 const mutations = {
-  registerStart(state) {
+  [mutationTypes.registerStart](state) {
     state.isSubmitting = true;
     state.validationErrors = null;
   },
-  registerSuccess(state, payload) {
+  [mutationTypes.registerSuccess](state, payload) {
     state.isSubmitting = false;
     state.currentUser = payload;
     state.isLoggedIn = true;
   },
-  registerFailure(state, payload) {
+  [mutationTypes.registerFailure](state, payload) {
+    state.isSubmitting = false;
+    state.validationErrors = payload;
+  },
+  [mutationTypes.loginStart](state) {
+    state.isSubmitting = true;
+    state.validationErrors = null;
+  },
+  [mutationTypes.loginSuccess](state, payload) {
+    state.isSubmitting = false;
+    state.currentUser = payload;
+    state.isLoggedIn = true;
+  },
+  [mutationTypes.loginFailure](state, payload) {
     state.isSubmitting = false;
     state.validationErrors = payload;
   }
 }
 
 const actions = {
-  register(contex, credentials) {
+  [actionTypes.register](contex, credentials) {
     return new Promise(resolve => {
-      contex.commit('registerStart');
+      contex.commit(mutationTypes.registerStart);
       authAPI
         .register(credentials)
         .then(response => {
-          contex.commit('registerSuccess', response.data.user);
+          contex.commit(mutationTypes.registerSuccess, response.data.user);
           setItem('accessToken', response.data.user.token);
           resolve(response.data.user);
         })
         .catch(result => {
-          contex.commit('registerFailure', result.response.data.errors);
+          contex.commit(mutationTypes.registerFailure, result.response.data.errors);
+        })
+    })
+  },
+
+  [actionTypes.login](contex, credentials) {
+    return new Promise(resolve => {
+      contex.commit(mutationTypes.loginStart);
+      authAPI
+        .login(credentials)
+        .then(response => {
+          contex.commit(mutationTypes.loginSuccess, response.data.user);
+          setItem('accessToken', response.data.user.token);
+          resolve(response.data.user);
+        })
+        .catch(result => {
+          contex.commit(mutationTypes.loginFailure, result.response.data.errors);
         })
     })
   }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,23 +3,15 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign Up</h1>
+          <h1 class="text-xs-center">Sign In</h1>
           <p class="text-xs-center">
-            <router-link :to="{name: 'login'}">Have an account?</router-link>
+            <router-link :to="{name: 'register'}">Need an account?</router-link>
           </p>
           <McvValidationErrors
             v-if="validationErrors"
             :validationErrors="validationErrors"
           />
           <form @submit.prevent="onSubmit">
-            <fieldset class="form-group">
-              <input
-                type="text"
-                class="form-control form-control-lg"
-                placeholder="Username"
-                v-model="username"
-              >
-            </fieldset>
 
             <fieldset class="form-group">
               <input
@@ -42,7 +34,7 @@
             <button class="btn btn-lg btn-primary pull-xs-right"
               :disabled="isSubmitting"
             >
-              Sign Up
+              Sign In
             </button>
           </form>
         </div>
@@ -57,15 +49,14 @@ import { actionTypes } from '../store/modules/auth';
 import { mapState } from 'vuex';
 
 export default {
-  name: 'McvRegister',
+  name: 'McvLogin',
   components: {
     McvValidationErrors
   },
   data () {
     return {
       email: '',
-      password: '',
-      username: ''
+      password: ''
     }
   },
   computed: {
@@ -82,13 +73,10 @@ export default {
   },
   methods: {
     onSubmit() {
-      console.log('submitted form');
-      this.$store.dispatch(actionTypes.register, {
+      this.$store.dispatch(actionTypes.login, {
         email: this.email,
-        username: this.username,
         password: this.password
-      }).then(user => {
-        console.log('successfully registered user', user);
+      }).then(() => {
         this.$router.push({name: 'home'});
       })
     }
