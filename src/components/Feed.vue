02620d1f88ae26841d14615dc8c5c54b9201eef6
ministrov <61511030+ministrov@users.noreply.github.com>
2023-03-15 13:49:13 +0300

    feat: render Feed component to GlobalFeed component from api data

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,10 +1,46 @@
 <template>
   <div>
-    <h2>Feed {{ apiUrl }}</h2>
+    <div v-if="isLoading">Loading...</div>
+
+    <div v-if="error">Something went wrong</div>
+
+    <div v-if="feed">
+      <div class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link :to="{name: 'userProfile', params: {slug: article.author.username}}">
+            <img :src="article.author.image" alt="">
+          </router-link>
+          <div class="info">
+            <router-link :to="{name: 'userProfile', params: {slug: article.author.username}}"
+              class="autor"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+            ADD TO FAVORITES
+          </div>
+        </div>
+        <router-link :to="{name: 'article', params: {slug: article.slug}}"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      PAGINATION
+    </div>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   import { actionTypes } from '@/store/modules/feed';
 
   export default {
@@ -14,6 +50,14 @@
         type: String,
         required: true
       }
+    },
+    computed: {
+      // Подписка на три поля , которые мы создали во Vuex(store)
+      ...mapState({
+        isLoading: state => state.feed.isLoading,
+        feed: state => state.feed.data,
+        error: state => state.feed.error,
+      })
     },
     mounted() {
       this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl});
