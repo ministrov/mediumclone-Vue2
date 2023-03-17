7dc83f6a5275f55b67e85649ec3319bf7b817837
ministrov <61511030+ministrov@users.noreply.github.com>
2023-03-17 11:32:07 +0300

    feat: create YourFeed view and TagFeed view components

---
+++ src/views/TagFeed.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home-page">
+    <McvBanner/>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <McvFeed :apiUrl="apiUrl" />
+        </div>
+        <div class="col-md-3">
+          <McvPopularTags/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import McvFeed from '@/components/Feed.vue'
+import McvPopularTags from '@/components/PopularTags'
+import McvBanner from '@/components/Banner'
+  export default {
+    name: 'McvYourFeed',
+    components: {
+      McvFeed,
+      McvPopularTags,
+      McvBanner
+    },
+    computed: {
+      apiUrl() {
+        const tagName = this.$route.params.slug;
+
+        return `/articles?tag=${tagName}`;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/YourFeed.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home-page">
+    <McvBanner/>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <McvFeed :apiUrl="apiUrl" />
+        </div>
+        <div class="col-md-3">
+          <McvPopularTags/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import McvFeed from '@/components/Feed.vue'
+import McvPopularTags from '@/components/PopularTags'
+import McvBanner from '@/components/Banner'
+  export default {
+    name: 'McvYourFeed',
+    components: {
+      McvFeed,
+      McvPopularTags,
+      McvBanner
+    },
+    data() {
+      return {
+        apiUrl: '/articles/feed'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
